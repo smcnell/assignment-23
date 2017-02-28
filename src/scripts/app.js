6a79d60d7d9f7efd29f8386208221f6c26428df3
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-02-28 14:59:00 -0500

    initial commit

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,60 @@
 import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const AllLegislatorsProp=React.createClass({
+
+  render: function(){
+    console.log(this.props.myLegislators)
+    return (
+      <div>
+      {this.props.myLegislators.map(function(singleCong, i){
+        return <SingleLegislator listofLegs={singleCong} index={i} key={i}/>
+      })}
+      </div>
+
+    )
+  }
+})
+
+const SingleLegislator=React.createClass({
+
+
+  render: function(){
+    console.log(this.props.listofLegs)
+
+    let legList=this.props.listofLegs
+    return (
+      <div className="one-person">
+        <div className="leg-heading">
+          <h3>{legList.first_name} {legList.last_name}</h3>
+          <h4>{legList.title} {legList.party}</h4>
+        </div>
+
+        <ul className="list">
+          <li>email: {legList.oc_email}</li>
+          <li>website: {legList.website}</li>
+          <li>facebook: {legList.facebook_id}</li>
+          <li>twitter: {legList.twitter_id}</li>
+        </ul>
+
+        <div className="footer">
+          <h7>Term End: {legList.term_end}</h7>
+        </div>
+      </div>
+
+    )
+  }
+})
+
+
+
+
+
+
+$.getJSON('https://congress.api.sunlightfoundation.com/legislators?callback=?').then(function(serverRes){
+  console.log(serverRes.results)
+  ReactDOM.render(
+    <AllLegislatorsProp myLegislators={serverRes.results}/> ,
+    document.querySelector('#app-container'));
+})
--- webpack.config.js
+++ webpack.config.js
@@ -13,28 +13,28 @@ module.exports = {
 			 exclude: /node_modules/,
 			 loader: 'babel-loader',
           query: {
-             presets: ['es2015']
+             presets: ['es2015', 'react']
           }
 		 },
 		 {
 		    test: /\.scss$/,
-			 loader: ExtractTextPlugin.extract({fallbackLoader: "style-loader", loader: "css-loader!sass-loader"})		 
+			 loader: ExtractTextPlugin.extract({fallbackLoader: "style-loader", loader: "css-loader!sass-loader"})
 			 },
 	 ]
   },
   plugins: [
      //uglify js
      new webpack.optimize.UglifyJsPlugin({
-			compress: { warnings: false }, 
+			compress: { warnings: false },
 			output: {comments: false},
          sourceMap: true
 	  }),
-	
+
      //env plugin
 	  new webpack.DefinePlugin({
         'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
 	  }),
-	 
+
      //env plugin -- css
      new ExtractTextPlugin({filename: './dist/css/styles.css', allChunks: true})
   ]
